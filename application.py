4bd35f6b292feaf3b7c57956ca8e043b7bc4bc64
Amaya Lucas <amayajlucas@gmail.com>
2024-01-08 12:16:49 -0500

    renaming app instances to application for eb deployment

--- application.py
+++ application.py
@@ -9,15 +9,15 @@ from models.building import Building
 
 from resources import user, building, unit
 
-app = Flask(__name__)
-api = Api(app)
+application = Flask(__name__)
+api = Api(application)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/affordwell_db"
-app.config['SQLALCHEMY_ECHO'] = True # Set to false for prod
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/affordwell_db"
+application.config['SQLALCHEMY_ECHO'] = True # Set to false for prod
 
-db.init_app(app)
-migrate = Migrate(app, db)
+db.init_app(application)
+migrate = Migrate(application, db)
 
 api.add_resource(user.Users, '/users')
 api.add_resource(building.Buildings, '/buildings')
@@ -26,5 +26,5 @@ api.add_resource(unit.Units, '/units')
 api.add_resource(unit.UnitDetail, '/units/<int:unit_id>')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    # application.run(debug=True)
+    application.run()
