850b45b4c97504683ffc643f05e43f1dd8f264dc
Amaya Lucas <amayajlucas@gmail.com>
2024-01-08 15:59:01 -0500

    testing basic deployment, no functionality

--- application.py
+++ application.py
@@ -10,20 +10,23 @@ from models.building import Building
 from resources import user, building, unit
 
 application = Flask(__name__)
-api = Api(application)
+@application.route("/")
+def hello_world():
+    return "Hello, World!"
+# api = Api(application)
 
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/affordwell_db"
-application.config['SQLALCHEMY_ECHO'] = True # Set to false for prod
+# application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# application.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/affordwell_db"
+# application.config['SQLALCHEMY_ECHO'] = True # Set to false for prod
 
-db.init_app(application)
-migrate = Migrate(application, db)
+# db.init_app(application)
+# migrate = Migrate(application, db)
 
-api.add_resource(user.Users, '/users')
-api.add_resource(building.Buildings, '/buildings')
-api.add_resource(building.BuildingDetail, '/buildings/<int:building_id>')
-api.add_resource(unit.Units, '/units')
-api.add_resource(unit.UnitDetail, '/units/<int:unit_id>')
+# api.add_resource(user.Users, '/users')
+# api.add_resource(building.Buildings, '/buildings')
+# api.add_resource(building.BuildingDetail, '/buildings/<int:building_id>')
+# api.add_resource(unit.Units, '/units')
+# api.add_resource(unit.UnitDetail, '/units/<int:unit_id>')
 
 if __name__ == '__main__':
     # application.run(debug=True)
