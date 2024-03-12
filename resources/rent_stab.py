9b6791e7676c2a08f53e818b2c5cc44369ff7c3d
Amaya Lucas <amayajlucas@gmail.com>
2024-03-12 15:03:36 -0400

    updated api calls for rent_stab

--- resources/rent_stab.py
+++ resources/rent_stab.py
@@ -3,45 +3,52 @@ from flask import request
 from models.rent_stab import RentStab
 from models.db import db
 from sqlalchemy.orm import joinedload
-from flask import jsonify
 from models.pluto import PLUTO
 
+def json_rentstab(rent_stab):
+    return {
+        'id': rent_stab.id,
+        'ucbbl': rent_stab.ucbbl,
+        'uc2018': rent_stab.uc2018,
+        'pdfsoa2018': rent_stab.pdfsoa2018,
+        'uc2019': rent_stab.uc2019,
+        'pdfsoa2019': rent_stab.pdfsoa2019,
+        'uc2020': rent_stab.uc2020,
+        'pdfsoa2020': rent_stab.pdfsoa2020,
+        'uc2021': rent_stab.uc2021,
+        'pdfsoa2021': rent_stab.pdfsoa2021,
+    }
+
+def json_pluto(pluto):
+    if pluto:
+        return {
+            'unitsres': pluto.unitsres,
+            'unitstotal': pluto.unitstotal,
+            'yearbuilt': pluto.yearbuilt,
+            'latitude': pluto.latitude,
+            'longitude': pluto.longitude
+        }
+    return None
 
 class RentStabs(Resource):
     def get(self):
-        data = RentStab.find_all()
-        return [rs.json() for rs in data]
-        # result = [row._asdict() for row in data]
-        # return jsonify(result)
+        # data = RentStab.find_all()
+        # return [rs.json() for rs in data]
+        data = db.session.query(RentStab, PLUTO).outerjoin(PLUTO, RentStab.ucbbl == PLUTO.bbl).all()
+
+        if data:
+            results = [{**json_rentstab(rent_stab), **json_pluto(pluto)} for rent_stab, pluto in data]
+            return results
+        else:
+            return {'message': 'No data found'}, 404
     
 class RentStabDetails(Resource):
     def get(self, bbl):
-        # data = RentStab.find_by_bbl(ucbbl=bbl)
-        # result = data.json()
-        # return result
-        result = db.session.query(RentStab, PLUTO).outerjoin(PLUTO, RentStab.ucbbl == PLUTO.bbl).filter(RentStab.ucbbl == bbl).first()
+        data = db.session.query(RentStab, PLUTO).outerjoin(PLUTO, RentStab.ucbbl == PLUTO.bbl).filter(RentStab.ucbbl == bbl).first()
 
-        if result:
-            rent_stab, pluto = result
-            return jsonify({
-                'id': rent_stab.id,
-                'ucbbl': rent_stab.ucbbl,
-                'uc2018': rent_stab.uc2018,
-                'pdfsoa2018': rent_stab.pdfsoa2018,
-                'uc2019': rent_stab.uc2019,
-                'pdfsoa2019': rent_stab.pdfsoa2019,
-                'uc2020': rent_stab.uc2020,
-                'pdfsoa2020': rent_stab.pdfsoa2020,
-                'uc2021': rent_stab.uc2021,
-                'pdfsoa2021': rent_stab.pdfsoa2021,
-                'pluto': {
-                    'unitsres': pluto.unitsres if pluto else None,
-                    'unitstotal': pluto.unitstotal if pluto else None,
-                    'yearbuilt': pluto.yearbuilt if pluto else None,
-                    'latitude': pluto.latitude if pluto else None,
-                    'longitude': pluto.longitude if pluto else None,
-                }
-            })
+        if data:
+            rent_stab, pluto = data
+            return {**json_rentstab(rent_stab), **json_pluto(pluto)}
         else:
             return {'message': 'No data found'}, 404
     
